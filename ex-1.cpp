f784b6eaf24bb0ab412f368db56b8ac42015e1ed
stealthscooty <bishgogoi@gmail.com>
2020-07-27 14:54:25 +0200

    This is initial commit

---
+++ ex-1.cpp
@@ -0,0 +1,10 @@
+//example 1 string output
+#include <iostream>
+using namespace std;
+
+
+int main()
+ {
+	cout<<"This is a String Output in C++"<<endl;
+	return 0;
+ }
---
+++ hello.cpp
@@ -0,0 +1,9 @@
+//hello.cpp
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	cout <<"Welcome back Bishwajit!"<<endl;
+	return 0;
+}
